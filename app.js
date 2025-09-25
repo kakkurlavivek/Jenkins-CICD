93f72b494b826b60da2754c29918481943fe574c
KAKKURLA VIVEK <85694902+kakkurlavivek@users.noreply.github.com>
2025-09-25 17:04:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => res.send('Hello from Node.js app!'));
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+const http = require('http');
+const port = 8080;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Dockerized Jenkins Pipeline!1');
+});
+server.listen(port);
